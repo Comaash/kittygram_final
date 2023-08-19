49beabfb13440353a5021b86fd5d3db85251ae0c
Анастасия Андреева <Comaash@yandex.ru>
2023-08-19 20:15:33 +0300

    media dir changed

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -103,7 +103,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/backend_media'
+MEDIA_ROOT = '/media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
