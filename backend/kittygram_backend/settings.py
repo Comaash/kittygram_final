e81dbc9f01f7b7996e1961d384b48f62093a865c
Анастасия Андреева <Comaash@yandex.ru>
2023-08-19 20:05:39 +0300

    media root fixed

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -103,7 +103,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/backend_media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
